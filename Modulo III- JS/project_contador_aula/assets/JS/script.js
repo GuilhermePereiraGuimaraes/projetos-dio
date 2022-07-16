dc04821e6bde57483769e3cced99ad157130d331
GuilhermePereiraGuimaraes <guilhermezemo@gmail.com>
2022-07-16 05:58:48 -0300

    concluido

--- Modulo III- JS/project_contador_aula/assets/JS/script.js
+++ Modulo III- JS/project_contador_aula/assets/JS/script.js
@@ -1,9 +1,53 @@
 var currentNumber = document.getElementById("currentNumber")
+var numCN = Number(currentNumber.innerText)
 
-function increment() {
-    currentNumber.innerText = Number(currentNumber.innerText) + 1
+function increment(bAdd) {
+    currentNumber.innerText = numCN + 1
+    numCN = Number(currentNumber.innerText)
+    let b = document.getElementById("sub")
+
+    checagem(false, b)
+    if (numCN>=10) {
+        checagem(true, bAdd)
+    } else {
+        checagem(false, bAdd)
+    }
+    corTexto()
 }
-function decrement() {
-    currentNumber.innerText = Number(currentNumber.innerText) - 1
+function decrement(bSub) {
+    currentNumber.innerText = numCN - 1
+    numCN = Number(currentNumber.innerText)
+    let b = document.getElementById("add")
     
+    checagem(false, b)
+    if (numCN<=-10) {
+        checagem(true, bSub)
+    } else {
+        checagem(false, bSub)
+    }
+    corTexto()
+}
+
+function checagem(key, b) {
+    if (key == true) {
+        b.disabled =true
+        b.style.color = "gray"
+        b.style.backgroundColor = "lightgray"
+        b.style.borderColor = "gray"
+    } else {
+        b.disabled =false
+        b.style.color = "lightcoral"
+        b.style.backgroundColor = "#f1d6d6"
+        b.style.borderColor = "lightcoral"
+    }
+}
+
+function corTexto() {
+    if (numCN > 0) {
+        currentNumber.style.color = "green"
+    } else if (numCN < 0) {
+        currentNumber.style.color = "red"
+    } else {
+        currentNumber.style.color = "black"
+    }
 }
