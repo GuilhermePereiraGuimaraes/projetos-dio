567f309a2a35dd7c3ce926b892cdd5c6bf88dd60
GuilhermePereiraGuimaraes <guilhermezemo@gmail.com>
2022-07-30 17:00:25 -0300

    Create test1.js

---
+++ Modulo III- JS/Funções/testes/test1.js
@@ -0,0 +1,24 @@
+(
+    function () {
+    let nome = "Guilherme"
+    return console.log(nome)
+    }
+)()
+
+const SOMA3 = (
+    function (a, b) {
+        return a+b
+    }
+
+)(12,3)
+
+console.log(SOMA3)
+
+function op(operacao, x, y) {
+    return operacao(x,y)
+}
+function multiplicacao(x,y) {
+    return x*y
+}
+
+console.log(op(multiplicacao, 3, 4))
