34ac6eb1cd5e1449d1db8909a6122468b88d525a
GuilhermePereiraGuimaraes <guilhermezemo@gmail.com>
2022-08-06 21:55:12 -0300

    Update script.js

--- Modulo III- JS/Debug/desafio_res/script.js
+++ Modulo III- JS/Debug/desafio_res/script.js
@@ -4,7 +4,32 @@ function validaArray(arr, num) {
         if (typeof(arr) !== 'object') throw new TypeError("O array precisa ser do tipo Object")
         if (typeof(num) !== 'number') throw new TypeError("O número precisa ser do tipo Number")
         if (arr.length !== num) throw new RangeError("Tamanho inválido")
+
+        return arr 
     } catch (e) {
-        
+        if (e instanceof ReferenceError) {
+            console.log("Esse erro é um ReferenceError")
+            console.log(e.name)
+            console.log(e.stack)
+            throw e
+        }
+        else if(e instanceof TypeError){
+            console.log("Esse erro é um TypeError")
+            console.log(e.name)
+            console.log(e.message)
+            console.log(e.stack)
+            throw e
+        }
+        else if(e instanceof RangeError){
+            console.log("Esse erro é um RangeError")
+            console.log(e.name)
+            console.log(e.message)
+            console.log(e.stack)
+            throw e
+        }
+        else{
+            console.log(`Tipo de erro não esperado: ${e}`)
+        }
     } 
 }
+console.log(validaArray([3,2,1],3))
