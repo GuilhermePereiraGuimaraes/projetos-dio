3fa3e10a4bb3bce05a751262aeafa9d2660dff44
GuilhermePereiraGuimaraes <guilhermezemo@gmail.com>
2022-08-16 21:52:59 -0300

    a

---
+++ Modulo III- JS/OO/desafio_res/script.js
@@ -0,0 +1,64 @@
+class ContaBancaria{
+    constructor (agencia, numero, tipo) {
+        this.agencia = agencia
+        this.numero = numero
+        this.tipo = tipo
+        this._saldo = 0 
+    }
+    get saldo(){
+        return this._saldo
+    }
+    set saldo(valor){
+        this._saldo = valor
+    }
+    sacar(valor) {
+        if (valor > this._saldo) {
+            return "Operação Negada!"
+        }
+        this._saldo -= valor
+
+        return this._saldo
+    }
+    depositar(valor) {
+
+        this._saldo += valor
+        return this._saldo
+    }
+}
+
+class ContaCorrente extends ContaBancaria{
+    constructor(agencia, numero, cartao){
+        super(agencia, numero)
+        this.tipo = 'Corrente'
+        this._cartao = cartao
+    }
+    get cartao(){
+        return this._cartao
+    }
+    set cartao(valor){
+        this._cartao = valor
+    }
+}
+
+class ContaPoupanca extends ContaBancaria{
+    constructor(agencia, numero){
+        super(agencia, numero)
+        this.tipo = 'Poupança'
+    }
+}
+class ContaUniversitaria extends ContaBancaria{
+    constructor(agencia, numero){
+        super(agencia, numero)
+        this.tipo = 'Universitária'
+    }
+
+    sacar(valor){
+        if (valor > 500 || valor > this._saldo) {
+            return 'Operação negada'
+        }
+
+        this._saldo -= valor
+
+        return saldo
+    }
+}
